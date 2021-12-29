1a2a4409a786778e230ceab22ba048bde08b47ff
jeff mascarenhas <jeftar.carvalho@tc.com.br>
2021-12-29 17:41:06 -0300

    feat: ensure load web3, accounts and connect the contract

--- client/src/app.js
+++ client/src/app.js
@@ -5,7 +5,6 @@ import TodoContract from "./contracts/Todo.json";
 import { TodoList } from "./components";
 
 function App() {
-  const [loadingApplication, setLoadingApplication] = useState(false);
   const [loading, setLoading] = useState(false);
   const [state, setState] = useState({
     web3: null,
@@ -13,19 +12,10 @@ function App() {
     accounts: null,
     currentAccount: null,
   });
-  const [todoList, setTodoList] = useState([]);
-  const [hasNotBalance, setHasNotBalance] = useState(false);
+
   const [formState, setForm] = useState({
     description: "",
   });
-
-  const handleCheckAccountBalance = (balance) => {
-    if (Number(balance) <= 0) {
-      setHasNotBalance(true);
-    } else {
-      setHasNotBalance(false);
-    }
-  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -38,7 +28,6 @@ function App() {
   useEffect(() => {
     const loadWeb3 = async () => {
       try {
-        setLoadingApplication(true);
         const web3 = await getWeb3();
         const accounts = await web3.eth.getAccounts();
         const networkId = await web3.eth.net.getId();
@@ -57,89 +46,20 @@ function App() {
         handleCheckAccountBalance(currentAccountBalance);
 
         setState({ web3, contract, accounts, currentAccount: currentAccount });
-
-        setLoadingApplication(false);
       } catch (error) {
         alert(
           `Failed to load web3, accounts, or contract. Check console for details`
         );
-        setLoadingApplication(false);
         console.error(error);
       }
     };
     loadWeb3();
   }, []);
 
-  useEffect(() => {
-    const { web3 } = state;
-
-    if (window.ethereum !== undefined && web3) {
-      window.ethereum.on("accountsChanged", async (accounts) => {
-        const currentAccount = accounts[0];
-        const currentAccountBalance = await web3.eth.getBalance(currentAccount);
-        handleCheckAccountBalance(currentAccountBalance);
-      });
-    }
-  }, [state]);
-
-  useEffect(() => {
-    const { contract } = state;
-    const loadTodoList = async () => {
-      try {
-        setLoading(true);
-        const taskCount = await contract.methods.taskCount().call();
-
-        const todos = [];
-
-        for (let i = 1; i <= taskCount; i++) {
-          const todoList = await contract.methods.tasks(i).call();
-          todos.push({
-            id: todoList.id,
-            description: todoList.description,
-            completed: todoList.completed,
-          });
-        }
-
-        setTodoList(todos);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        console.error(error);
-      }
-    };
-    if (contract) {
-      loadTodoList();
-    }
-  }, [state.contract]);
-
-  const handleAddTask = async (value) => {
-    const { contract, currentAccount } = state;
-
-    try {
-      setLoading(true);
-      const response = await contract.methods.addTask(value).send({
-        from: currentAccount,
-      });
-
-      const { id, completed, description } =
-        response.events.AddTaskCreated.returnValues;
-
-      const newTask = { id, completed, description };
-
-      const todoListUpdated = [...todoList, newTask];
-
-      setLoading(false);
-      setTodoList(todoListUpdated);
-    } catch (error) {
-      setLoading(false);
-      console.log("addTask_error: ", error);
-    }
-  };
-
   const handleSubmitTask = (event) => {
     event.preventDefault();
     const value = event.target.firstChild.value;
-    handleAddTask(value);
+    console.log(value);
   };
 
   return (
@@ -157,21 +77,7 @@ function App() {
           />
           <button className="app-button">+</button>
         </form>
-        <br />
-        <TodoList todoList={todoList} />
-        {loading && (
-          <div className="task-loading">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        )}
       </div>
-      {hasNotBalance && state.currentAccount && (
-        <div className="alert alert--warn">
-          This account doesn't has balance
-        </div>
-      )}
     </>
   );
 }
