3056ff4eea6e3a0625dea5b42bd82d8d77961861
jeff mascarenhas <jeftar.carvalho@tc.com.br>
2022-01-03 18:51:16 -0300

    feat: actions creation to Todo Context

---
+++ client/src/contexts/actions.js
@@ -0,0 +1,9 @@
+const actions = {
+  CONNECT_TO_ETHEREUM_NETWORK: "todoList/CONNECT_TO_ETHEREUM_NETWORK",
+  LOADING_APPLICATION: "todoList/LOADING_APPLICATION",
+  LOAD_TODO_LIST: "todoList/LOAD_TODO_LIST",
+  ADD_TASK: "todoList/ADD_TASK",
+  COMPLETED_TOGGLE: "todoList/COMPLETED_TOGGLE",
+};
+
+export default actions;
