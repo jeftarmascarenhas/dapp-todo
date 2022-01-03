2cd65193e8903468905224cd132051ca54f1f945
jeff mascarenhas <jeftar.carvalho@tc.com.br>
2022-01-03 18:47:26 -0300

    feat: create a simple Loading component

---
+++ client/src/components/loading/loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div className="task-loading">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default Loading;
