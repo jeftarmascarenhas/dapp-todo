0a478608d320ae1ddc587ca6f7c5901f8fa2ca66
jeff mascarenhas <jeftar.carvalho@tc.com.br>
2022-01-03 18:45:00 -0300

    refactor: ensure App use state of context and use especific css

--- client/src/app.js
+++ client/src/app.js
@@ -1,71 +1,21 @@
 import { useContext, useEffect, useState } from "react";
 import "./app.css";
-import getWeb3 from "./services/web3-service";
-import TodoContract from "./contracts/Todo.json";
-import { Loading, TodoList } from "./components";
-import { TodoContext, actions } from "./contexts";
+import { Loading, TodoList, Form } from "./components";
+import { TodoContext } from "./contexts";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [state, setState] = useState({
-    web3: null,
-    contract: null,
-    accounts: null,
-    currentAccount: null,
-  });
-  const { dispatch } = useContext(TodoContext);
+  const { state } = useContext(TodoContext);
 
   const [hasNotBalance, setHasNotBalance] = useState(false);
-  const [formState, setForm] = useState({
-    description: "",
-  });
 
   const handleCheckAccountBalance = (balance) => {
+    console.log(balance);
     if (Number(balance) <= 0) {
       setHasNotBalance(true);
     } else {
       setHasNotBalance(false);
     }
   };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setForm({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  useEffect(() => {
-    const loadWeb3 = async () => {
-      try {
-        const web3 = await getWeb3();
-        const accounts = await web3.eth.getAccounts();
-        const networkId = await web3.eth.net.getId();
-
-        // Get contract address
-        const deployedNetwork = TodoContract.networks[networkId];
-
-        const contract = new web3.eth.Contract(
-          TodoContract.abi,
-          deployedNetwork && deployedNetwork.address
-        );
-
-        const currentAccount = accounts[0];
-
-        const currentAccountBalance = await web3.eth.getBalance(currentAccount);
-        handleCheckAccountBalance(currentAccountBalance);
-
-        setState({ web3, contract, accounts, currentAccount: currentAccount });
-      } catch (error) {
-        alert(
-          `Failed to load web3, accounts, or contract. Check console for details`
-        );
-        console.log("loading web3 error: ", error);
-      }
-    };
-    loadWeb3();
-  }, []);
 
   useEffect(() => {
     const { web3 } = state;
@@ -85,57 +35,14 @@ function App() {
     }
   }, [state]);
 
-  const handleAddTask = async (value) => {
-    const { contract, currentAccount } = state;
-
-    try {
-      setLoading(true);
-      const response = await contract.methods.addTask(value).send({
-        from: currentAccount,
-      });
-
-      const { id, completed, description } =
-        response.events.AddTaskCreated.returnValues;
-
-      const newTask = { id, completed, description };
-
-      dispatch({
-        type: actions.ADD_TASK,
-        payload: newTask,
-      });
-
-      setLoading(false);
-      // setTodoList(todoListUpdated);
-    } catch (error) {
-      setLoading(false);
-      console.log("addTask_error: ", error);
-    }
-  };
-
-  const handleSubmitTask = (event) => {
-    event.preventDefault();
-    const value = event.target.firstChild.value;
-    handleAddTask(value);
-  };
-
   return (
     <>
       <div className="app">
         <h1 className="app-title">Todo List - Blockchain</h1>
-        <form className="app-form" onSubmit={handleSubmitTask}>
-          <input
-            name="description"
-            id="description"
-            className="app-input"
-            placeholder="Write the new task"
-            value={formState.description}
-            onChange={handleChange}
-          />
-          <button className="app-button">+</button>
-        </form>
+        <Form />
         <br />
         {<TodoList contract={state.contract} />}
-        {loading && <Loading />}
+        {state.loading && <Loading />}
       </div>
       {hasNotBalance && state.currentAccount && (
         <div className="alert alert--warn">
